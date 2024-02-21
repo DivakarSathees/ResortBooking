99491253b1a964f5406d7b70d406452531532bdf
neojarvis <techadmin@iamneo.ai>
2024-02-21 03:48:42 +0000

    Auto-save-commit-21/02/2024--09:18:39

--- angularapp/src/app/models/user.model.ts
+++ angularapp/src/app/models/user.model.ts
@@ -2,7 +2,7 @@ export class User {
     userId?: number;
     email: string;
     password: string;
-    username: string;
-    mobileNumber: string;
-    userRole: string;
+    username?: string;
+    mobileNumber?: string;
+    userRole?: string;
 }
